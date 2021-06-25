3694be08aec93bfb4fe99d7ee84ced0f176ee09d
Max Severin <makc.severin@gmail.com>
2021-06-25 15:36:45 +0000

    Add GET /api/convert endpoint

--- routes/api.js
+++ routes/api.js
@@ -7,4 +7,32 @@ module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
 
+  app.get('/api/convert', function (req, res) {
+    const initNum = convertHandler.getNum(req.query.input);
+    const initUnit = convertHandler.getUnit(req.query.input);
+
+    if (initNum === 'invalid number' && initUnit === 'invalid unit'){
+      res.status(200).json('invalid number and unit');
+    }
+
+    if (initNum === 'invalid number'){
+      res.status(200).json(initNum);
+    }
+
+    if (initUnit === 'invalid unit'){
+      res.status(200).json(initUnit);
+    }
+
+    const returnNum = convertHandler.convert(initNum, initUnit);
+    const returnUnit = convertHandler.getReturnUnit(initUnit);
+
+    res.status(200).json({
+      initNum,
+      initUnit,
+      returnNum,
+      returnUnit,
+      "string": convertHandler.getString(initNum, initUnit, returnNum, returnUnit),
+    });
+  });
+
 };
